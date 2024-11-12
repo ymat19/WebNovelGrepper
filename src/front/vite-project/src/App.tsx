86d90ff956d8341c5ec4d2227e022c3d9b33e9a1
mtbyk <mtbyk@mtbyk-air.local>
2024-11-13 02:48:12 +0900

    emptyなキャッシュがヒット、キャッシュがない、を区別

--- src/front/vite-project/src/App.tsx
+++ src/front/vite-project/src/App.tsx
@@ -101,32 +101,32 @@ const App: React.FC = () => {
     );
 
     // キャッシュ取得
-    const getCache = async (hash: string): Promise<Record[]> => {
+    const getCache = async (hash: string): Promise<[boolean, Record[]]> => {
       try {
         const cacheResponse = await fetch(
           `cache/${parser.workId}/${hash}.json`
         );
         if (cacheResponse.ok) {
-          return await cacheResponse.json();
+          return [true, await cacheResponse.json()];
         } else if (
           cacheResponse.status === 404 ||
           cacheResponse.status === 403
         ) {
           // キャッシュが存在しない 本来404だが、Cloud Frontが403を返す
-          return [];
+          return [false, []];
         } else {
           throw new Error(`Failed to fetch: ${cacheResponse.statusText}`);
         }
       } catch (err) {
         // ローカルデバッグだと別ファイルをとってきて SyntaxError
-        if (err instanceof SyntaxError) return [];
+        if (err instanceof SyntaxError) return [false, []];
         throw err;
       }
     };
 
     // 最初にキャッシュチェック あれば終わり
-    const cache = await getCache(hash);
-    if (cache.length) return cache;
+    const [hit, cache] = await getCache(hash);
+    if (hit) return cache;
 
     // キャッシュがない場合 クエリを投げる
     const queryResponse = await fetch(
@@ -140,8 +140,8 @@ const App: React.FC = () => {
       // eslint-disable-next-line @typescript-eslint/no-unused-vars
       for (const _ of Array(10).keys()) {
         await new Promise((resolve) => setTimeout(resolve, 5000));
-        const cache = await getCache(hash);
-        if (cache.length) return cache;
+        const [hit, cache] = await getCache(hash);
+        if (hit) return cache;
       }
     }
 
