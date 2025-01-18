1b78a45c0dc69e6f60c494cccdc175478c2d3bda
mtbyk <mtbyk@mtbyk-air.local>
2025-01-19 08:47:56 +0900

    オプションからbase指定

--- src/front/vite-project/vite.config.ts
+++ src/front/vite-project/vite.config.ts
@@ -1,14 +1,8 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths"
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-  console.log(env.VITE_PLATFORM === 'gh-pages' ? '/WebNovelGrepper/' : '/')
-
-  return {
-    plugins: [react(), tsconfigPaths()],
-    base: 'hoge', // モードで切り替え
-  };
-});
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+})
