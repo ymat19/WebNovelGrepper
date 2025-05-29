c3d99e5f93077922ecfee118e57eea790bc3fa3e
ymat19 <ymat19@example.com>
2025-05-30 06:31:04 +0900

    chore: add devshells

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in
+      {
+        packages.default = pkgs.mkShell {
+          packages = with pkgs; [
+            python310
+            nodejs_24
+            terraform
+          ];
+        };
+      }
+    );
+}
